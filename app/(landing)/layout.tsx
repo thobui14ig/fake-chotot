968a5c5ffa5f61e97d47cdb03878f0160458f9c7
thobui14ig <buithanhtho14ig@gmail.com>
2023-08-04 00:00:30 +0700

    spin

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,16 +1,29 @@
+'use client'
 import HeaderLanding from "@/components/landing/navbar/navbar"
 import Carousel from "../../components/landing/carousel"
 import Category from "@/components/landing/category"
 import './layout.scss'
+import { useEffect, useState } from "react"
+import { Spin } from 'antd';
 
 const HomeLayout = ({
     children,
   }: {
     children: React.ReactNode
   }) => {
+    const [isSpin, setIsSpin] = useState<boolean>(true)
+
+    useEffect(() => {
+      setTimeout(() => {
+        setIsSpin(false)
+      }, 2000);
+    }, [])
+
     return(
       <main>
         <div className="flex flex-col flex-1">
+        {isSpin ? <Spin/> : 
+          <>
             <HeaderLanding/>
             <main className="pt-20 ">
               <div className="main">
@@ -19,11 +32,12 @@ const HomeLayout = ({
                 {children}
               </div> 
                 
-            </main>
+            </main>          
+          </>
+        }
+
         </div>
-      </main>
-
-
+      </main>    
     )
 }
 export default HomeLayout
